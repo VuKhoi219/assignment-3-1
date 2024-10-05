3ded58a6498330f9ef20d5748c7955689553766e
MinhThach1908 <pandaminh2008@gmail.com>
2024-10-05 15:02:56 +0700

    tự động lấy tin bản đầu tiên ( gần hoàn thiện )

---
+++ models/news.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const newsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+    },
+    source: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const News = mongoose.model("News", newsSchema);
+
+module.exports = News;
